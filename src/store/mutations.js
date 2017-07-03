7e9cbae88d6f814f4272a3d084fc44a553ddd3ab
huangc <hc_youxiang@sina.cn>
2017-07-03 23:38:59 +0800

    edit

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,4 +3,10 @@
  */
 import modules from '@/modules'
 import {GET_USER_INFO} from './mutation-types'
-
+import {getUserInfo} from '@/service/AjaxEngin'
+export default {
+  [GET_USER_INFO](state){
+      let res=this.getUserInfo()
+      return res.data
+  }
+}
